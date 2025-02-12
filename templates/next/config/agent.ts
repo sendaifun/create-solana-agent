f1626207bcb9102c0b7fe0f45ac6a10c0a5a6df1
shivaji43 <shivajijraut@gmail.com>
2025-02-13 01:30:30 +0530

    fix: corrected openAI model name

--- templates/next/config/agent.ts
+++ templates/next/config/agent.ts
@@ -10,7 +10,7 @@ import { ChatDeepSeek } from "@langchain/deepseek";
 export async function initializeAgent(modelName: string) {
   const llm = modelName?.includes("OpenAI") 
     ? new ChatOpenAI({
-        modelName: "gpt-4o-turbo",
+        modelName: "gpt-4o-mini",
         temperature: 0.3,
         apiKey: process.env.OPENAI_API_KEY!,
       })
